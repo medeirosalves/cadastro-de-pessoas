fa3ba8d8715b022548d6807fe020e1dbfd9d3b06
medeirosalves <medeirosalves.me@gmail.com>
2026-06-28 18:10:38 -0300

    feat: adiciona atributo a classe pessoa como ArrayList<String>

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,7 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Pessoa {
 
     private String nome;
     private int idade;
+    private ArrayList<String> pessoa = new ArrayList<>();
 
     public Pessoa(String nome, int idade) {
         this.nome = nome;
