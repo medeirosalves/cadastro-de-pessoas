70bbfa1cbc450513216077974e9b99dbc3b2650e
medeirosalves <medeirosalves.me@gmail.com>
2026-06-28 18:03:56 -0300

    feat: adiciona o menu interativo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao;
+
+        do {
+            System.out.println("""
+                    ( 1 ) Cadastrar pessoa
+                    ( 2 ) Listar pessoas
+                    ( 3 ) Remover pessoa
+                    ( 4 ) Sair
+                    """);
+            opcao = scanner.nextInt();
+
+            if(opcao == 4) {
+                System.out.println("Encerrando o programa...");
+            }
+
+        } while (opcao != 4);
 
     }
 }
